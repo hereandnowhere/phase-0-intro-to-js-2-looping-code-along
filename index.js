db514e40ae0b60084722eae4119dfc7636be56e4
Jacob Vine <jvine1235@gmail.com>
2022-01-17 15:25:51 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+const names = [`Guadalupe`, `Ollie`, `Aki`];
+
+const party = "suprise"
+
+function writeCards(guests, event) {
+  for (let i = 0; i < guests.length; i++) {
+    guests.splice(i,1,`Thank you, ${guests[i]}, for the wonderful ${event} gift!`);
+  }
+
+  return guests;
+}
+
+writeCards(names, party);
+
+function countDown(num){
+    let countDown = num;
+    while (countDown >= 0){
+        console.log(countDown);
+        countDown--;
+    }
+    return num
+}
